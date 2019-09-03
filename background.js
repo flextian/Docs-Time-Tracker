28e835c87155810d816aec543524e0460b2d45ec
alex t <sloopooicecream@gmail.com>
2019-09-02 21:58:33 -0400

    locked and afk do not count time

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-undef */
-console.log("working");
 chrome.tabs.onActivated.addListener(tab_switch);
 chrome.windows.onFocusChanged.addListener(window_switch);
 chrome.tabs.onUpdated.addListener(url_switch);
+chrome.idle.onStateChanged.addListener(activity);
+chrome.idle.setDetectionInterval(15)
 
-prevUrl = null;
+setInterval(function(){console.log("CURRENT TIME " + globalTimers[tabUrl]); }, 1000);
+
+var tabUrl;
+var tabId;
+var currentTimers = {};
+var globalTimers = {};
+var prevTabUrl = null;
 
 function url_switch(){
 	tab_window_switched();
@@ -16,19 +23,58 @@ function window_switch(){
 	tab_window_switched();
 }
 
+var currentState;
+function activity(){
+	chrome.idle.queryState(15, function(status){
+		currentState = status;
+		//idle start
+		if (tabUrl.includes("/edit") && (status == "idle" || status == "locked")){
+			console.log("idle start");
+			var timePassed = (new Date() - currentTimers[tabUrl]) / 1000;
+			currentTimers[tabUrl] = new Date();
+			globalTimers[tabUrl] += timePassed;
+		}
+		//idle stop
+		if (tabUrl.includes("/edit") && status == "active"){
+			console.log("idle stop");
+			currentTimers[tabUrl] = new Date();
+			chrome.tabs.sendMessage(tabId, {time: globalTimers[tabUrl]});
+		}
+	});
+}
+
 function tab_window_switched(){
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabArray) {
 		var tabData = tabArray[0];
-		var tabUrl;
 		if (tabData == null){
 			tabUrl = "none";
 		}
 		else{
-			tabUrl = tabData.url; 
+			tabUrl = tabData.url;
+			tabId = tabData.id;
 		}
-		if (prevUrl != tabUrl){
-			console.log(tabUrl);
-			prevUrl = tabUrl;
+
+
+		//if tab switched out (stop)
+		if (prevTabUrl != null && prevTabUrl.includes("/edit") && currentState != "idle" && currentState != "locked"){
+			console.log("stop");
+			//time passed calculations
+			var timePassed = (new Date() - currentTimers[prevTabUrl]) / 1000;
+			currentTimers[tabUrl] = new Date();
+			if (!(prevTabUrl in globalTimers)){
+				globalTimers[prevTabUrl] = 0;
+			}
+			globalTimers[prevTabUrl] += timePassed;
 		}
+		//if tab switched on (start)
+		if (tabUrl.includes("/edit") && currentState != "idle"){
+			console.log("start");
+			currentTimers[tabUrl] = new Date();
+			chrome.tabs.sendMessage(tabId, {time: globalTimers[tabUrl]});
+		}
+		prevTabUrl = tabUrl;
 	})
 }
+
+//fix case where if u switch fast after unafking it counts the time afk
+//make other programs count as afk? (while docs is open in the back)
--- content.js
+++ content.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-undef */
-var docUrl = window.location.pathname;
-var active = docUrl.includes("edit");
-
-if(active){
-	console.log("lets go");
+console.log("esketit");
+chrome.runtime.onMessage.addListener(got_message);
+var documentTime = "Time: ";
+var html = '<div class="docs-title-widget goog-inline-block" id="docs-title-widget timer-thing"><div class="docs-title-input-label" style="pointer-events: none; max-width: 621px;"><span id ="sbs">' + documentTime + '</span></div></div>';
+$('.docs-activity-indicator-container').append(html);
+function got_message(message){
+    var hours = Math.floor(message.time/3600);
+    var minutes = Math.floor((message.time - hours * 3600)/60);
+    var seconds = Math.floor(message.time % 60);
+    document.getElementById("sbs").innerHTML = hours + " Hours " + minutes + " Minutes " + seconds + " Seconds";
 }
+// fix the on all tabs close case - afk case (5 min time out)
