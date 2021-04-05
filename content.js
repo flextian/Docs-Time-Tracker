b64a9895c66e8b8f5bbe517eff3e825f5f4f3d30
flextian <sloopooicecream@gmail.com>
2021-04-05 01:54:38 -0400

    polished code, regrouped code into methods

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-undef */
-console.log("esketit");
-chrome.runtime.onMessage.addListener(got_message);
-var documentTime = "Time: ";
-var html = '<div class="docs-title-widget goog-inline-block" id="docs-title-widget timer-thing"><div class="docs-title-input-label" style="pointer-events: none; max-width: 621px;"><span id ="sbs">' + documentTime + '</span></div></div>';
+chrome.runtime.onMessage.addListener(update_time);
+var html = '<div class="docs-title-widget goog-inline-block" id="docs-title-widget timer-thing"><div class="docs-title-input-label" style="pointer-events: none; max-width: 621px;"><span id ="time-span"></span></div></div>';
 $('.docs-activity-indicator-container').append(html);
-function got_message(message){
-    var hours = Math.floor(message.time/3600);
-    var minutes = Math.floor((message.time - hours * 3600)/60);
+
+// After json is sent to doc tab
+function update_time(message){
+    console.log(message)
+    var hours = Math.floor(message.time / 3600);
+    var minutes = Math.floor((message.time - hours * 3600) / 60);
     var seconds = Math.floor(message.time % 60);
-    document.getElementById("sbs").innerHTML = hours + " Hours " + minutes + " Minutes " + seconds + " Seconds";
+    document.getElementById("time-span").innerHTML = hours + " Hours " + minutes + " Minutes " + seconds + " Seconds";
 }
 // fix the on all tabs close case - afk case (5 min time out)
