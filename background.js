80bac1cf57b5b3e74ce0da401bfb486f70cdd9ea
flextian <sloopooicecream@gmail.com>
2021-07-03 18:00:02 -0400

    implimented local storage

--- background.js
+++ background.js
@@ -1,8 +1,20 @@
 var tabUrl;
 var tabId;
-var timeStorage = {};
 var currentState = "active";
 var visibilityState = "visible";
+
+// Check to see if local storage already exists
+chrome.storage.sync.get(["timeStorage"], function(items){
+
+	console.log(items['timeStorage']);
+
+	if (items['timeStorage'] === undefined) {
+		chrome.storage.sync.set({ "timeStorage": {} }, function(){});
+	}
+
+});
+
+
 
 setInterval(function(){
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabArray) {
@@ -19,29 +31,40 @@ setInterval(function(){
 			}
 
 			if (isGoogleDoc(tabUrl)){
-				if (!(tabUrl in timeStorage)){
-					timeStorage[tabUrl] = 0;
-				}
 				
-				chrome.idle.queryState(30, function(status){
-					currentState = status;
+				// If docs tab is not in storage
+				chrome.storage.sync.get(["timeStorage"], function(items){				
+
+					if (!(tabUrl in items['timeStorage'])){
+						items['timeStorage'][tabUrl] = 0;
+					}
+
+					chrome.idle.queryState(30, function(status){
+						currentState = status;
+					});
+
+					chrome.tabs.sendMessage(tabId, {visibilityRequest: "true"}, function(visibility){
+						visibilityState = visibility;
+					});
+
+					console.log(visibilityState);
+
+					if(currentState == 'active' && visibilityState == 'visible'){
+						items['timeStorage'][tabUrl] += 1;
+					}
+
+					chrome.tabs.sendMessage(tabId, {time: items['timeStorage'][tabUrl], state: currentState});
+
+					chrome.storage.sync.set({ "timeStorage": items['timeStorage'] }, function(){});
+
 				});
 				
-				chrome.tabs.sendMessage(tabId, {visibilityRequest: "true"}, function(visibility){
-					visibilityState = visibility;
-				});
-
-				console.log(visibilityState);
-				if(currentState == 'active' && visibilityState == 'visible'){
-					timeStorage[tabUrl] += 1;
-				}
-
-				chrome.tabs.sendMessage(tabId, {time: timeStorage[tabUrl], state: currentState});
-
 			}
 
-			console.log(timeStorage);
-
+			// For debugging, print timeStorage every second
+			chrome.storage.sync.get(["timeStorage"], function(items){
+				console.log(items['timeStorage']);			
+			});
 
 		}
 	)}, 1000);
